e7dc4a582115ff4c27c10245cb507017c46e6cfb
Garcia-Joao <amorimisme@gmail.com>
2026-05-10 11:25:26 -0300

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -35,9 +35,12 @@ const allowedOrigins = [
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
   'http://192.168.15.4:3001',
+
   'https://panelordr.com.br',
   'https://www.panelordr.com.br',
+  'https://app.panelordr.com.br',
   'https://admin.panelordr.com.br',
+
   process.env.FRONTEND_URL,
   process.env.FRONTEND_LAN_URL,
   process.env.ADMIN_FRONTEND_URL,
